7194597bd1bec224826ad870ca7c08027923aed4
forestlaw77 <87220249+forestlaw77@users.noreply.github.com>
2023-06-14 22:02:25 +0900

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+import Board from "./Board";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Simple Othello Game</h1>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
